e1dcd23d343b1ce43622d4996b0880859384734f
TraineeBweb <trainee.bweb@gmail.com>
2025-11-13 17:07:03 +0530

    made changes in th home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from "react";
-import Slider from 'react-slick';
+import Slider from "react-slick";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Button from 'react-bootstrap/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
 import { IoIosArrowRoundDown } from "react-icons/io";
 // import video from "../assets/img/video-img.png";
 import video from "../assets/img/video-thumb.png";
@@ -35,10 +35,10 @@ import { Link } from "react-router-dom";
 import Timer from "./Timer";
 import RegisterYourself from "./RegisterYourself";
 import CardContent from "./CardContent";
-
+import "../assets/css/style.css";
+import { FiUsers, FiTrendingUp, FiBriefcase, FiUser } from "react-icons/fi";
 
 export default function Home() {
-
   const handleScroll = () => {
     window.scrollTo({
       top: document.getElementById("target-section").offsetTop,
@@ -54,7 +54,7 @@ export default function Home() {
     autoplay: true,
     speed: 500,
     autoplaySpeed: 4000,
-    cssEase: 'linear',
+    cssEase: "linear",
     responsive: [
       {
         breakpoint: 1024, // Screen width less than 1024px
@@ -85,37 +85,36 @@ export default function Home() {
   const handleShow = () => setShow(true);
 
   const gallery = [
-    { url: require('../assets/img/Sponser logo/1.png') },
-    { url: require('../assets/img/Sponser logo/2.png') },
-    { url: require('../assets/img/Sponser logo/3.png') },
-    { url: require('../assets/img/Sponser logo/4.png') },
-    { url: require('../assets/img/Sponser logo/5.png') },
-    { url: require('../assets/img/Sponser logo/6.png') },
-    { url: require('../assets/img/Sponser logo/7.png') },
-    { url: require('../assets/img/Sponser logo/8.png') },
-    { url: require('../assets/img/Sponser logo/9.png') },
-    { url: require('../assets/img/Sponser logo/11.png') },
-    { url: require('../assets/img/Sponser logo/12.png') },
-    { url: require('../assets/img/Sponser logo/13.png') },
-    { url: require('../assets/img/Sponser logo/14.png') },
-    { url: require('../assets/img/Sponser logo/15.png') },
-    { url: require('../assets/img/Sponser logo/16.png') },
-    { url: require('../assets/img/Sponser logo/17.png') },
-    { url: require('../assets/img/Sponser logo/18.png') },
-    { url: require('../assets/img/Sponser logo/19.png') },
-    { url: require('../assets/img/Sponser logo/20.png') },
-    { url: require('../assets/img/Sponser logo/21.png') },
-    { url: require('../assets/img/Sponser logo/22.png') },
-    { url: require('../assets/img/Sponser logo/23.png') }
-  ]
+    { url: require("../assets/img/Sponser logo/1.png") },
+    { url: require("../assets/img/Sponser logo/2.png") },
+    { url: require("../assets/img/Sponser logo/3.png") },
+    { url: require("../assets/img/Sponser logo/4.png") },
+    { url: require("../assets/img/Sponser logo/5.png") },
+    { url: require("../assets/img/Sponser logo/6.png") },
+    { url: require("../assets/img/Sponser logo/7.png") },
+    { url: require("../assets/img/Sponser logo/8.png") },
+    { url: require("../assets/img/Sponser logo/9.png") },
+    { url: require("../assets/img/Sponser logo/11.png") },
+    { url: require("../assets/img/Sponser logo/12.png") },
+    { url: require("../assets/img/Sponser logo/13.png") },
+    { url: require("../assets/img/Sponser logo/14.png") },
+    { url: require("../assets/img/Sponser logo/15.png") },
+    { url: require("../assets/img/Sponser logo/16.png") },
+    { url: require("../assets/img/Sponser logo/17.png") },
+    { url: require("../assets/img/Sponser logo/18.png") },
+    { url: require("../assets/img/Sponser logo/19.png") },
+    { url: require("../assets/img/Sponser logo/20.png") },
+    { url: require("../assets/img/Sponser logo/21.png") },
+    { url: require("../assets/img/Sponser logo/22.png") },
+    { url: require("../assets/img/Sponser logo/23.png") },
+  ];
 
-  const text =`<p class="para">
+  const text = `<p class="para">
               Snehshilp Foundation's "Start-Up Fest Gujarat" highlights the thriving spirit of entrepreneurship and innovation in the region. Founded and driven by the visionary Mrs. Snehal Brahmbhatt, the event aims to fill the void in start-up support by providing a comprehensive platform for fresh ideas to flourish.
               </p>
               <p class="para">
               The annual event brings together start-ups, investors, and industry collaboration. Leaders for two days, creating an atmosphere of inspiration and fruitful collaboration.
-              </p>`
-
+              </p>`;
 
   return (
     <>
@@ -123,16 +122,15 @@ export default function Home() {
         <Container>
           <Row className="justify-content-center">
             <Col lg={12}>
-            <img src={mainBanner} className="w-100"/>
+              <img src={mainBanner} className="w-100" />
             </Col>
-          
           </Row>
         </Container>
       </section>
       <section className="top-banner">
         <Container>
           <Row>
-          <Col lg={4} md={5} sm={12} >
+            <Col lg={4} md={5} sm={12}>
               <div className=" p-relative">
                 <img src={video} className="w-100" />
                 <a onClick={handleShow} className="play-btn"></a>
@@ -175,81 +173,90 @@ export default function Home() {
                 </Link>
               </div>
             </Col>
-           
           </Row>
         </Container>
       </section>
 
       <section className="padding-sec light-bg about-sec">
-      <CardContent about={about} text={text}  title="About StartUp Fest"  buttonShow={true} to="/about" linkToTitle="Learn More"/>
-
+        <CardContent
+          about={about}
+          text={text}
+          title="About StartUp Fest"
+          buttonShow={true}
+          to="/about"
+          linkToTitle="Learn More"
+        />
       </section>
 
       <section className="padding-sec">
-      <Timer />
+        <Timer />
       </section>
 
       <div className="container-bg">
         <RegisterYourself />
       </div>
 
-      <section className="padding-sec ">
-        <Container>
-          <Row>
-            <Col lg={6} xs={12} className="order-lg-0 order-1">
-              <Row className="">
-                <Col lg={5} xs={6}>
-                  <div className="col-card2">
-                    <div className="check">
-                      <FaRegCheckCircle />
-                    </div>
-                    <p>1000+</p>
-                    <h4>Participants</h4>
-                  </div>
-                </Col>
-                <Col lg={5} xs={6}>
-                  <div className="col-card2">
-                    <div className="check">
-                      <FaRegCheckCircle />
-                    </div>
+      <section className="padding-sec">
+        <div
+          className="stats-bg">
+          <Container>
+            <Row>
+              <Col>
+                <h3 className="title mb-5 text-center">
+                  Empowering India’s{" "}
+                  <span className="title-gradient">Startup Ecosystem</span>
+                </h3>
+              </Col>
+            </Row>
 
-                    <p>200+</p>
-                    <h4>Startups</h4>
-                  </div>
-                </Col>
+            <Row>
+              {/* Left: Stats Cards */}
+              <Col lg={6} xs={12} className="order-lg-0 order-1">
+                <Row>
+                  {[
+                    {
+                      value: "1000+",
+                      label: "Participants",
+                      icon: <FiUsers size={32} color="#d28b2a" />,
+                    },
+                    {
+                      value: "200+",
+                      label: "Startups",
+                      icon: <FiTrendingUp size={32} color="#4a85e6" />,
+                    },
+                    {
+                      value: "135+",
+                      label: "Investors",
+                      icon: <FiBriefcase size={32} color="#8b6de0" />,
+                    },
+                    {
+                      value: "15000+",
+                      label: "Visitors",
+                      icon: <FiUser size={32} color="#3bb5a1" />,
+                    },
+                  ].map((card, index) => (
+                    <Col lg={5} xs={6} key={index} className="mb-4">
+                      <div className="stat-card">
+                        <div className="icon-wrap">{card.icon}</div>
+                        <p className="stat-value">{card.value}</p>
+                        <h4 className="stat-label">{card.label}</h4>
+                      </div>
+                    </Col>
+                  ))}
+                </Row>
+              </Col>
 
-                <Col lg={5} xs={6}>
-                  <div className="col-card2">
-                    <div className="check">
-                      <FaRegCheckCircle />
-                    </div>
-
-                    <p>135+</p>
-                    <h4>Investors</h4>
-                  </div>
-                </Col>
-
-                <Col lg={5} xs={6}>
-                  <div className="col-card2">
-                    <div className="check">
-                      <FaRegCheckCircle />
-                    </div>
-
-                    <p>15000+</p>
-                    <h4>Visitors</h4>
-                  </div>
-                </Col>
-              </Row>
-            </Col>
-            <Col lg={6} xs={12} className="sfg order-lg-1 order-0">
-              <p className="font-blue">SFG 2024</p>
-              <h3 className="title">
-                India Startup Revolution is Here and now! At Ahmedabad December{" "}
-                <span className="gradient"> 13 & 14, 2025 </span>
-              </h3>
-            </Col>
-          </Row>
-        </Container>
+              {/* Right: SFG Text */}
+              <Col lg={6} xs={12} className="sfg order-lg-1 order-0">
+                <p className="font-blue">SFG 2024</p>
+                <h3 className="title">
+                  India Startup Revolution is Here and now! At Ahmedabad{" "}
+                  <span className="title-gradient">December 13 & 14, 2025</span>
+                </h3>
+              </Col>
+            </Row>
+          </Container>
+        </div>
       </section>
 
       <section className="container-bg padding-sec">
@@ -340,12 +347,12 @@ export default function Home() {
                 <Slider {...settings}>
                   {gallery.map((item, index) => (
                     <div className="adjust-height" key={index}>
-                    <img
-                      // key={index}
-                      src={item.url}
-                      alt={`Gallery Image ${index + 1}`}
-                    className="img-fluid "
-                    />
+                      <img
+                        // key={index}
+                        src={item.url}
+                        alt={`Gallery Image ${index + 1}`}
+                        className="img-fluid "
+                      />
                     </div>
                   ))}
                   {/* <img src={parti1} alt="participant" />
--- src/components/RegisterYourself.jsx
+++ src/components/RegisterYourself.jsx
@@ -1,107 +1,100 @@
-import React from 'react'
+import React from "react";
 import Container from "react-bootstrap/Container";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import { MdArrowOutward } from "react-icons/md";
-import img1 from "../assets/img/pitcher.jpeg";
-import img2 from "../assets/img/start-up.jpeg";
-import img3 from "../assets/img/glob.jpeg";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 export default function RegisterYourself() {
-  return (
-    <div>
-      <section className="padding-sec">
-        <Container>
-          <Row className='align-items-center'>
-            <Col lg={8} md={9} xs={6}>
-              <h3 className="title">Register Yourself</h3>
-            </Col>
-            <Col lg={4} md={3} xs={6} className="text-end">
-              <Link to="/register" className="theme-btn">
-                Register Now
-              </Link>
-            </Col>
-            <Col lg={6} xs={12}>
-              <h5 className="para">
-                People who register for the Start-Up Fest will be allotted a
-                5-minute time slot to present their startup in front of
-                potential investors.
-              </h5>
-            </Col>
-            {/* <Col className="flex-shrink-0 text-end">
-              <Link to="/register" className="theme-btn">Register Now</Link>
-            </Col> */}
-          </Row>
-          <Row className="justify-content-between">
-            <Col lg={3} md={3} xs={12} className="col-card">
-              <Link to="/register" className="no-design">
-                <div className="name-line pitcher-card-div">
-                  <h3>
-                    <Link to="/register" className="pitcher-card-a">
-                      Pitcher
-                    </Link>
-                  </h3>
-                  <Link href="/register" className="pitcher-card-a">
-                    <MdArrowOutward/>
-                  </Link>
-                </div>
-                <p>
-                  Pitchers will get a 3 by 3 stall and a chance to&nbsp;
-                  <span className="yellow-bg">present their project/idea/</span>
-                  &nbsp; startup on stage in front of all the investors.
-                </p>
-                <img src={img1} className="w-100" />
-              </Link>
-            </Col>
-            <Col lg={3} md={3} xs={12} className="col-card">
-              <Link to="/register" className="no-design">
-                <div className="name-line pitcher-card-div">
-                  <h3>
-                    <Link to="/register" className="pitcher-card-a">
-                      Startup
-                    </Link>
-                  </h3>
-                  <Link to="/register" className="pitcher-card-a">
-                    <MdArrowOutward/>
-                  </Link>
-                </div>
-                <p>
-                  Startups/ franchises will get a 2/2 stall and&nbsp;
-                  <span className="yellow-bg">
-                    visibility in front of all the investors
-                  </span>
-                  &nbsp; coming in.
-                </p>
-                <img src={img2} className="w-100" />
-              </Link>
-            </Col>
+  const cards = [
+    {
+      title: "Pitcher",
+      text: (
+        <>
+          Pitchers will get a 3 by 3 stall and a chance to{" "}
+          <span className="yellow-bg">present their project/idea/</span> startup
+          on stage in front of all the investors.
+        </>
+      ),
+    },
+    {
+      title: "Startup",
+      text: (
+        <>
+          Startups/franchises will get a 2/2 stall and{" "}
+          <span className="yellow-bg">visibility in front of all investors</span>{" "}
+          coming in.
+        </>
+      ),
+    },
+    {
+      title: "Investors",
+      text: (
+        <>
+          Investors get access to all startups and an opportunity to{" "}
+          <span className="yellow-bg">multiply their money by 2–3x.</span>
+        </>
+      ),
+    },
+    {
+      title: "Sponsors",
+      text: (
+        <>
+          Sponsors get{" "}
+          <span className="yellow-bg">
+            premier visibility and direct access
+          </span>{" "}
+          to high-growth founders & capital-ready investors.
+        </>
+      ),
+    },
+  ];
 
-            <Col lg={3} md={3} xs={12} className="col-card">
-              <Link to="/register" className="no-design">
-                <div className="name-line pitcher-card-div">
-                  <h3>
-                    <Link to="/register" className="pitcher-card-a">
-                      Investors
-                    </Link>
-                  </h3>
-                  <Link to="/register" className="pitcher-card-a">
-                    <MdArrowOutward/>
-                  </Link>
+  return (
+    <section className="padding-sec">
+      <Container>
+        <Row className="align-items-center">
+          <Col lg={8} md={9} xs={6}>
+            <h3 className="title">Register Yourself</h3>
+          </Col>
+          <Col lg={4} md={3} xs={6} className="text-end">
+            <Link to="/register" className="theme-btn">
+              Register Now
+            </Link>
+          </Col>
+
+          <Col lg={6} xs={12}>
+            <h5 className="para">
+              People who register for the Start-Up Fest will be allotted a
+              5-minute time slot to present their startup in front of
+              potential investors.
+            </h5>
+          </Col>
+        </Row>
+
+        {/* Cards */}
+        <Row className="justify-content-between row-margin">
+          {cards.map((card, i) => (
+            <Col lg={3} md={3} xs={12} key={i}>
+              {/* ONE SINGLE LINK WRAPS THE FULL CARD */}
+              <Link to="/register" className="no-design card-link-wrapper">
+                <div className="col-card">
+                  <div className="name-line pitcher-card-div">
+                    <h3 className="card-title">{card.title}</h3>
+
+                    {/* Arrow — NOT a <Link>, but clickable because parent Link wraps everything */}
+                    <span className="arrow-icon">
+                      <MdArrowOutward />
+                    </span>
+                  </div>
+
+                  <p>{card.text}</p>
                 </div>
-                <p>
-                  Investors will get a chance to talk to all the startups and an
-                  opportunity to &nbsp;
-                  <span className="yellow-bg">
-                    multiply their money by 2-3 times.
-                  </span>
-                </p>
-                <img src={img3} className="w-100" />
               </Link>
             </Col>
-          </Row>
-        </Container>
-      </section>
-    </div>
-  )
+          ))}
+        </Row>
+      </Container>
+    </section>
+  );
 }
